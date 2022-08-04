f34f8e2d37a491c92960a4fe2789829dc494063d
anumide <alabasamuelayomide3@gmail.com>
2022-08-04 23:13:24 +0100

    responsiveness implemented

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,23 +1,26 @@
 <template>
-	<header class="py-4 pl-8 pr-4 border-b border-[#bdbdbd] border-solid flex justify-between items-center">
+	<header class="lg:py-4 py-2 lg:pl-8 pl-4 lg:pr-4 pr-2 border-b border-[#bdbdbd] border-solid flex justify-between items-center">
 		<div class="font-medium text-lg">
 			Beauty Care Company
 		</div>
-		<div class="profile-notification flex">
-			<div class="notification w-20 flex justify-between items-center p-3 border-[#adadad] border-solid border-r">
+		<div class="profile-notification md:flex hidden">
+			<div class="notification lg:w-20 w-16 flex justify-between items-center lg:p-3 p-2 border-[#adadad] border-solid border-r">
 				<icon name="notification" class="w-5 text-[#292D32] fill-transparent" />
 				<icon name="category" class="w-5 text-[#292D32] fill-transparent" />
 			</div>
-			<div class="profile w-56 flex p-3 items-center justify-between">
-				<div class="profile-image w-10 h-10 text-center rounded-full overflow-hidden text-[#292D32] border border-[#292D32] flex justify-between items-center">
+			<div class="profile lg:w-52 w-48 flex lg:p-3 p-2 items-center justify-between">
+				<div class="profile-image w-8 h-8 text-center rounded-full overflow-hidden text-[#292D32] border border-[#292D32] flex justify-between items-center">
 					<!-- <img src="@/assets/image/Traq.png" alt=""> -->
 					BA
 				</div>
 				<p class="font-medium">
 					Bayo Adenekan
 				</p>
-				<icon name="arrowDown" class="w-5 text-[#667085] fill-transparent" />
+				<icon name="arrowDown" class="w-3 text-[#667085] fill-transparent" />
 			</div>
+		</div>
+		<div class="hamburger block md:hidden w-[8%]">
+			<icon name="hamburger" class="w-full text-[#292D32] fill-transparent" />
 		</div>
 	</header>
 </template>
--- src/components/sideNav.vue
+++ src/components/sideNav.vue
@@ -1,52 +1,52 @@
 <template>
-	<div class="lg:w-80 w-60 h-full text-white bg-[#201A62]">
-		<div class="py-6 lg:py-8">
-			<div class="logo flex text-neutral-50 pl-6 lg:pl-12">
-				<icon name="hamburger" class="w-5 text-neutral-50" />
-				<img src="@/assets/image/Traq.png" alt="">
+	<div class="lg:w-80 w-60 h-full text-white bg-[#201A62] md:block hidden">
+		<div class="py-4 lg:py-8">
+			<div class="logo flex items-center text-neutral-50 pl-4 lg:pl-12">
+				<icon name="hamburger" class="w-8 text-neutral-50" />
+				<img src="@/assets/image/Traq.png" alt="" class="w-[40%]">
 			</div>
 			<!-- side nav menu lists -->
 			<div class="menu-list my-8 lg:my-12">
-				<div class="dashboard pl-6 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between">
+				<div class="dashboard pl-4 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between hover:bg-[#3488ED] hover:bg-opacity-10 transition">
 					<icon name="category" class="w-5 mr-2 fill-transparent" />
 					<span class="grow">Dashboard</span>
 				</div>
-				<div class="sales pl-6 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between">
+				<div class="sales pl-4 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between hover:bg-[#3488ED] hover:bg-opacity-10 transition">
 					<icon name="sales" class="w-5 mr-2 fill-transparent" />
 					<span class="grow">Sales</span>
 				</div>
-				<div class="expense pl-6 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between">
+				<div class="expense pl-4 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between hover:bg-[#3488ED] hover:bg-opacity-10 transition">
 					<icon name="expense" class="w-5 mr-2 fill-transparent" />
 					<span class="grow">Expense</span>
 				</div>
-				<div class="bank pl-6 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between">
+				<div class="bank pl-4 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between hover:bg-[#3488ED] hover:bg-opacity-10 transition">
 					<icon name="bank" class="w-5 mr-2 fill-transparent" />
 					<span class="grow">Bank</span>
 				</div>
-				<div class="accountant pl-6 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between">
+				<div class="accountant pl-4 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between hover:bg-[#3488ED] hover:bg-opacity-10 transition">
 					<icon name="accountant" class="w-5 mr-2 fill-transparent" />
 					<span class="grow">Accountant</span>
 				</div>
-				<div class="tax pl-6 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between">
+				<div class="tax pl-4 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between hover:bg-[#3488ED] hover:bg-opacity-10 transition">
 					<icon name="tax" class="w-5 mr-2 fill-transparent" />
 					<span class="grow">Tax</span>
 				</div>
-				<div class="reports pl-6 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between">
+				<div class="reports pl-4 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between hover:bg-[#3488ED] hover:bg-opacity-10 transition">
 					<icon name="report" class="w-5 mr-2 fill-transparent" />
 					<span class="grow">Reports</span>
 				</div>
-				<div class="hrm pl-6 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between">
+				<div class="hrm pl-4 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between hover:bg-[#3488ED] hover:bg-opacity-10 transition">
 					<icon name="hrm" class="w-5 mr-2 fill-transparent" />
 					<span class="grow">HRM</span>
 				</div>
 			</div>
 			<!-- setting and logout -->
 			<div class="setting-logout">
-				<div class="setting pl-6 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between">
+				<div class="setting pl-4 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between hover:bg-[#3488ED] hover:bg-opacity-10 transition">
 					<icon name="setting" class="w-5 mr-2 fill-transparent" />
 					<span class="grow">Settings</span>
 				</div>
-				<div class="logout pl-6 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between">
+				<div class="logout pl-4 lg:pl-12 pr-2 font-medium text-neutral-50 py-2 my-1 flex items-center justify-between hover:bg-[#3488ED] hover:bg-opacity-10 transition">
 					<icon name="logout" class="w-5 mr-2 fill-transparent" />
 					<span class="grow">Logout</span>
 				</div>
