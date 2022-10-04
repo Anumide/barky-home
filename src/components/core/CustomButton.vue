63fb64ca99b853b15655f73c5e063ffe941755d3
anumide <alabasamuelayomide3@gmail.com>
2022-10-04 06:43:32 +0100

    created component for button and categories for dashboard

---
+++ src/components/core/CustomButton.vue
@@ -0,0 +1,16 @@
+<template>
+	<button class="border-solid border-primary border rounded text-xs" @click="$emit('click')">
+		{{ text }}
+	</button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    text: {
+        type: String,
+        required: true
+    }
+})
+
+defineEmits(['click'])
+</script>
---
+++ src/components/dashboard/category.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<div>
+			<img src="" alt="">
+		</div>
+		<h1>{{ categoryType }}</h1>
+		<p>{{ categoryText }}</p>
+		<CustomButton text="" />
+	</div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    categoryType: {
+        type: String,
+        required: true
+    },
+    categoryText: {
+        type: String,
+        required: true
+    }
+})
+</script>
